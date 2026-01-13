b83a9ded79c10a2960d4f1a5af3cda6bdf7375c3
Qazi Hamzah <qazihamzah7@gmail.com>
2026-01-13 23:54:46 +0530

    Add delete operation to Array ADT

--- arrays/array_adt.c
+++ arrays/array_adt.c
@@ -22,6 +22,7 @@ void insert(struct Array *arr,int index,int x){
     if (arr->length == arr->size) {
         return;
     }
+   
 int i;
     for (i = arr->length; i > index; i--) {
         arr->A[i] = arr->A[i - 1];
@@ -31,6 +32,21 @@ int i;
     arr->length++;
 }
 
+int Delete(struct Array *arr, int index)
+{
+    int i, x = 0;
+
+    if (index < 0 || index >= arr->length)
+        return 0;
+
+    x = arr->A[index];
+
+    for (i = index; i < arr->length - 1; i++)
+        arr->A[i] = arr->A[i + 1];
+
+    arr->length--;
+    return x;
+}
 
 
 int main() {
@@ -38,7 +54,8 @@ int main() {
     struct Array arr = {{1, 2, 3, 4, 5}, 10, 5};
     insert(&arr, 2, 10);
     display(arr);
-
+    Delete(&arr,3);
+    display(arr);
     return 0;
 }
 
